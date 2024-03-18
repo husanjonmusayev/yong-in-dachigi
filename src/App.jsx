211a647279602c058344476acf67dbb9bf6effb3
husanjonmusayev <husanjonm279@gmail.com>
2024-03-18 21:45:14 +0500

    go

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,11 @@ import { GoogleLogin } from "@react-oauth/google";
 import "./App.css";
 
 const clientId =
-  191503931009 - eqlrq4m79dknuiis7tli75c1dvgjkoom.apps.googleusercontent.com;
+  "191503931009 - eqlrq4m79dknuiis7tli75c1dvgjkoom.apps.googleusercontent.com";
 
 function App() {
   const handleLoginSuccess = (response) => {
-    console.log(response.profileObj); // Foydalanuvchi profil ma'lumotlariga kirish
-    // Foydalanuvchi ma'lumotlarini autentifikatsiya uchun serverga jo'natish yoki mahalliy saqlash uchun yuboring
+    console.log(10, response.profileObj);
   };
 
   const handleLoginFailure = (error) => {
